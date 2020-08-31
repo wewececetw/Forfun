361da7def55e430f6edd6d91698975124d99e4a2
barron <wewececetw@gmail.com>
2020-09-01 00:58:47 +0800

    正式上傳

---
+++ vendor/tcg/voyager/publishable/lang/es/login.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'loggingin'    => 'Iniciando sesión',
+    'signin_below' => 'Ingresar abajo:',
+    'welcome'      => 'Bienvenido a Voyager. El administrador que le faltaba a Laravel ',
+];
---
+++ vendor/tcg/voyager/src/Http/Middleware/VoyagerAdminMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TCG\Voyager\Http\Middleware;
+
+use Closure;
+
+class VoyagerAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!app('VoyagerAuth')->guest()) {
+            $user = app('VoyagerAuth')->user();
+            app()->setLocale($user->locale ?? app()->getLocale());
+
+            return $user->hasPermission('browse_admin') ? $next($request) : redirect('/');
+        }
+
+        $urlLogin = route('voyager.login');
+
+        return redirect()->guest($urlLogin);
+    }
+}
