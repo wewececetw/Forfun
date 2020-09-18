167a7bff29187340a79d43a5c613fca363b96738
barron <wewececetw@gmail.com>
2020-09-18 22:40:03 +0800

    加分題google串接後登入laravel內建會員系統

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -1,28 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Auth;
 use Socialite;
+use App\User;
+use DB;
+use Str;
 
 class SocialiteController extends Controller
 {
-/**
- * Redirect the user to the Google authentication page.
- *
- * @return 301 
- */
-public function redirectToProvider()
-{
-    return Socialite::driver('google')->redirect();
-}
+    /**
+     * Redirect the user to the Google authentication page.
+     *
+     * @return 301 
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
 
-/**
- * Obtain the user information from GitHub.
- *
- * @return \Illuminate\Http\Response
- */
-public function handleProviderCallback()
-{
-    $user = Socialite::driver('google')->user();
-    dd($user);
-}
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->user();
+        $apiToken = Str::random(10);
+        
+        $user_data = DB::select('select * from users where email = ?',[$user->email]);
+        
+        if(empty($user_data)){
+
+            $user_l_data = User::create([
+                                'name' => $user->name,
+                                'email' => $user->email,
+                                'password' => Hash::make($apiToken),
+                            ]);
+
+        }else{
+            $user_l_data = User::where('email', $user->email)->first();
+        }
+        Auth::login($user_l_data, true);
+        return view('home');
+    }
 }
